30eadb368f49e00701e09000aa02591f601a40b6
Vikkee1 <viljami.kontu@gmail.com>
2026-04-29 19:59:27 +0300

    Renamed queues

--- main/src/message_bus.c
+++ main/src/message_bus.c
@@ -5,38 +5,38 @@
 #define MSG_BUS_TAG "MSG_BUS_TAG"
 #define BUS_QUEUE_LEN 16
 
-static QueueHandle_t ble_to_can_q;
-static QueueHandle_t can_to_ble_q;
+static QueueHandle_t from_ble_q;
+static QueueHandle_t from_can_q;
 
 void message_bus_init(void)
 {
-    ble_to_can_q = xQueueCreate(16, sizeof(bus_msg_t));
-    can_to_ble_q = xQueueCreate(16, sizeof(bus_msg_t));
+    from_ble_q = xQueueCreate(16, sizeof(bus_msg_t));
+    from_can_q = xQueueCreate(16, sizeof(bus_msg_t));
 
-    configASSERT(ble_to_can_q);
-    configASSERT(can_to_ble_q);
+    configASSERT(from_ble_q);
+    configASSERT(from_can_q);
 }
 
 bool bus_publish_ble(const bus_msg_t *msg)
 {
-    return xQueueSend(ble_to_can_q, msg, 0) == pdTRUE;
+    return xQueueSend(from_ble_q, msg, 0) == pdTRUE;
 }
 
 bool bus_publish_can(const bus_msg_t *msg)
 {
-    return xQueueSend(can_to_ble_q, msg, 0) == pdTRUE;
+    return xQueueSend(from_can_q, msg, 0) == pdTRUE;
 }
 
 bool bus_subscribe_ble(bus_msg_t *msg, uint32_t timeout_ms)
 {
-    return xQueueReceive(can_to_ble_q,
+    return xQueueReceive(from_ble_q,
                          msg,
                          pdMS_TO_TICKS(timeout_ms)) == pdTRUE;
 }
 
 bool bus_subscribe_can(bus_msg_t *msg, uint32_t timeout_ms)
 {
-    return xQueueReceive(ble_to_can_q,
+    return xQueueReceive(from_can_q,
                          msg,
                          pdMS_TO_TICKS(timeout_ms)) == pdTRUE;
 }
